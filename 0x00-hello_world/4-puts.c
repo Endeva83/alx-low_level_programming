df942c31e28d3fba666462e7c035ddf15212567c
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-06-30 20:30:33 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point 
- *
- * Return: Always 0 (Success) 
- */
+* main -entry point
+* Return: Always 0 (sucess)
+*/
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-}
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+} 
