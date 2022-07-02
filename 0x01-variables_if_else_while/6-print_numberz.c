bbfd0840a1cf34bd2fb7b470cd31bbf079e5c39b
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-01 19:44:04 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+*main- entry point
+*Description: Alphabets
+*Return: zero on success
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
