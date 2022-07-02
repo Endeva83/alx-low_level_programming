1befed1fc605024fd7ce85d6da8285fa8ae6cdb2
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-01 21:47:24 -0700

    Task Extra 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+*main - writes 00 - 99
+*Decription: Writes all unique combinations
+*Return: zero for success
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
