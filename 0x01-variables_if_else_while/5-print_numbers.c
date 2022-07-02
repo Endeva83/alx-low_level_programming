b921f23e72665abf5d323f2cf98a946accefed7d
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-01 19:38:14 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+*main- entry point
+*Description: Alphabets
+*Return: zero on success
+*/
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
