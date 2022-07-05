501dd74221b92207c6e4a9395196b102e51ec3b5
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-05 14:49:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*main - Entry point
+*Description: Print _putchar and a newline
+*Return: zero on success
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
