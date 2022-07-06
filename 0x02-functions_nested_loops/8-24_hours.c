fc4e93ab990bfd0b1f87950b2f48d4c0a1c6f2ec
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-05 19:48:09 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*jack_bauer - prints all minutes of the day
+*@void: first int to add
+*/
+void jack_bauer(void)
+{
+	int hrs;
+
+	int mins;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
