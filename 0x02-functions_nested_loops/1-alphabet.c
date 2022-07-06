b21a0fbef9e4ff24db78265a6f1183f5ccb9b1e7
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-05 19:29:56 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet - Print the lowercase alphabet.
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
