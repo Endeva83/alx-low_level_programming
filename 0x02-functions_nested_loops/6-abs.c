76329942ab7c20965fce9ca2d70fb3bb819cde1e
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-05 19:43:48 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_abs - computes absolute value
+*@n: int to compute as absolute
+*Return: absolute value
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
