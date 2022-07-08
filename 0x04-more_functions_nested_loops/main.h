9424de101493268859434f4f9e34f0c2d0df9a62
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-07 17:42:00 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,53 @@
+#include <unistd.h>
+
+/**
+*_putchar - writes the character c to stdout
+*print_alphabet - prints alphabet and returns new line
+*print_alphabet_x10 - print alphabet 10 times
+*_islower - return 1 if c is lowercase, 0 otherwise
+*_isalpha - return 1 if c is upper or lowercase, 0 otherwise
+*print_sign - return 1 for positive, 0 for 0, and -1 for negative
+*_abs - computes absolute value of integer
+*print_last_digit - print last digit
+*add - add two ints, return sum
+*jack_bauer - prints all minutes of the day
+*times_table - prints times table up to 9
+*print_to_98 - prints all natural numbers to 98
+*print_times_table - print times table up to
+*_isupper - checks if char is capital letter
+*_isdigit - checks if int is a digit 0-9
+*mul - multiply two numbers
+*print_void - print numbers 0-9
+*print_most_numbers - print 0-9 w/out 2 and 4
+*more_numbers - print 0-14 ten times
+*print int using only putchar
+*print a line of n length
+*print a diagonal line of n length
+*print a square of n size
+*print a triangle of n size
+*/
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int);
+void print_times_table(int);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_number(int);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
