05bd16b66bd91846078f219437969428b7dcc78b
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-07 17:53:26 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*print_numbers - print 0-9
+*Return: nothing
+*/
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
