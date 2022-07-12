c1c89c770e1d2ec35cce67cdff06a1dae5c21c9b
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-12 10:24:56 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*reset_to_98 - set value of int to 98
+*@n: address of value to change
+*Description: resets *n to 98
+*Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
