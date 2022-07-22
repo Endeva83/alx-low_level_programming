7e58428eeb9ca6f65815925fe80471d3ec2b9136
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-22 15:45:21 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints name of file
+* @argc: number of arguments
+* @argv: arguments entered
+* Description: print name of file
+* Return: zero
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	argc = argc;
+
+	return (0);
+}
