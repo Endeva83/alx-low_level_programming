f323bdab4b664b18fa9f06811163e678b838c335
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-25 08:19:49 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Entry point
+ * @size: size of the array
+ * @c: character
+ * Return: a point to the array, or NUll if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array = NULL;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	if (size != 0)
+	{
+		array = (char *)malloc(size * sizeof(char));
+		if (array != NULL)
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+		}
+	}
+	return (array);
+}
