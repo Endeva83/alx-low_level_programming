b579bd93b150752b9545591b8f4c9479ab522850
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-07-28 07:52:01 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *  _calloc - Entry point
+ *  @nmemb: size of the array
+ *  @size: size of the type
+ *  Return: a pointer to an array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *array = NULL;
+	unsigned int i;
+	unsigned int t;
+	char *f;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	t = nmemb * size;
+	array = malloc(t);
+	f = (char *)array;
+	if (f != NULL)
+	{
+		for (i = 0; i < t; i++)
+			f[i] = 0;
+		return (f);
+	}
+	return (NULL);
+}
