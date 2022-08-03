3c6846bafefd08b61f32d8fe4bfde7e59e9a26f3
Adesubokan Solomon Niyi <endevagamesusa@gmail.com>
2022-08-03 10:06:03 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Print a name
+ * @name:name
+ * @f:ponter to function
+ * Return: no return
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
